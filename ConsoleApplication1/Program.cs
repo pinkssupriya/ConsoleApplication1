e29f7dc78adb18a230f16b1486f12edec493a74d
pinkssupriya <34835777+pinkssupriya@users.noreply.github.com>
2018-07-08 15:33:34 +0530

    upd dev (#1)
    merge from dev to master

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hai!");
+            Console.WriteLine("Hello!");
         }
     }
 }
