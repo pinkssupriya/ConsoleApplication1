03129d33f66a4b11675e56dcaca3ca5c247bd9b3
supriya007 <pinkssupriya@outlook.com>
2018-07-11 17:54:40 +0000

    test1

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hai!!!");
-           // Console.WriteLine("Hello!!!");
+            Console.WriteLine("Hello!!!");
         }
     }
 }
