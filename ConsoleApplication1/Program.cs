f303987d0371631853346334d9a11d1617b8bde6
pinkssupriya <34835777+pinkssupriya@users.noreply.github.com>
2018-07-20 07:51:48 +0000

    Update Program.cs

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-           // Console.WriteLine("Welcome!!!");
+           Console.WriteLine("Welcome!!!");
             Console.WriteLine("Hello!!!");
         }
     }
